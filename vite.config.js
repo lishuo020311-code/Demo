dd84decb5038feee35a4e9ac2411c69632bb116c
LenovolLS <2664162852@qq.com>
2025-12-04 16:30:13 +0800

    配置修改

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // 保持配置对象为空即可，因为您的项目目前不需要特殊配置
-    // Vite 默认的配置（如处理 .js, .css 文件）已经足够了。
-
-    // 您也可以添加一个基础路径配置，以防您在其他地方部署：
-    // base: './', 
-    base: '/demo/',
-    plugins: []
-});
+    server: {
+        host: true,
+        allowedHosts: ['*']
+    }
+})
